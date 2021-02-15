ce9f5b71f8c06023705db4c5ceb485e5c951ad4d
kostitsyn <kostitsin.a@mail.ru>
2021-02-15 13:40:18 +0400

    Второе домашнее задание

--- core.py
+++ core.py
@@ -1,6 +1,8 @@
 import sys
 import re
+import urllib
 sys.path.append('../')
+
 
 # Front controllers
 def set_key(request):
@@ -16,13 +18,32 @@ def user_authorize(request):
 
 
 class Application:
+    """Класс создающий объект для запуска сервера."""
     def __init__(self, routes, fronts):
         self.routes = routes
         self.fronts = fronts
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
-        print(path)
+        print(environ)
+        requests = {}
+
+        request_type = environ['REQUEST_METHOD']
+        print(f'Тип запроса: {request_type}')
+        requests['method'] = request_type
+
+        # Если тип запроса POST
+        if request_type == 'POST':
+            data = self.get_post_data(environ)
+            requests['data'] = data
+            # print(f'Полученные данные: {data}')
+
+        # Если тип запроса GET c параметрами
+        if environ["QUERY_STRING"]:
+            data = self.get_data(environ)
+            requests['data'] = data
+            # print(f'Полученные данные: {data}')
+
         check_html = len(re.findall(r'html.*', path))
         if check_html:
             check_slash = len(re.findall(r'/$', path))
@@ -39,8 +60,7 @@ class Application:
                 path = path.split()
                 path.append('/')
                 path = ''.join(path)
-        print(path)
-        requests = {}
+
         for front_view in self.fronts:
             front_view(requests)
         if path in self.routes:
@@ -50,4 +70,44 @@ class Application:
             code, text_response = '404 ERROR', 'Page not found!'
         start_response(code, [('Content-Type', 'text/html')])
         return [text_response.encode('utf-8')]
+
+    def get_post_data(self, environ: dict) -> dict:
+        """
+        Получить параметры POST запроса в виде словаря, где ключ - название
+        параметра, значение - соответствующее этому ключу значение.
+        :param environ:
+        :return:
+        """
+        contnent_length_data = environ['CONTENT_LENGTH']
+        content_length = int(contnent_length_data) if contnent_length_data\
+            else 0
+        string_bytes = environ['wsgi.input'].read(content_length) if\
+            content_length else b''
+        query_string = string_bytes.decode('utf-8')
+        data = self.parse_string(query_string)
+        return data
+
+    def get_data(self, environ: dict) -> dict:
+        """
+        Получить параметры GET запроса в виде словаря, где ключ - название
+        параметра, значение - соответствующее этому ключу значение.
+        :param environ:
+        :return:
+        """
+        query_string = environ['QUERY_STRING']
+        data = self.parse_string(query_string)
+        return data
+
+    def parse_string(self, string: str) -> dict:
+        """
+        Преобразовать строку с параметрами в словарь.
+        :param string:
+        :return:
+        """
+        data = urllib.parse.parse_qs(string, encoding='utf-8')
+        data = {key: value[0] for key, value in data.items()}
+        return data
+
+
+
 
--- templates.py
+++ templates.py
@@ -2,6 +2,12 @@ from jinja2 import Template
 
 
 def render(template_name, **kwargs):
+    """
+    Создать шаблонизатор для HTML страниц, используя jinja.
+    :param template_name:
+    :param kwargs:
+    :return:
+    """
     with open(f'templates/{template_name}', encoding='utf-8') as f:
         template = Template(f.read())
     return template.render(**kwargs)
